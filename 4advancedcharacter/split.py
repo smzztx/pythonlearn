4459851dffc9820363351c39ce22e14c236f9003
tx <tangxiang007@gmail.com>
2017-08-17 08:22:33 -0700

    add 4advancedcharacter,split.py

---
+++ 4advancedcharacter/split.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+L=['michael','sarah','tracy','bob','jack']
+print L
+print L[0:3]
+print L[:3]
+print L[1:3]
+print L[:-2]
+
+T=(1,2,3,4,5,6,7,8,9,10)
+print T
+print T[:3]
+
+
