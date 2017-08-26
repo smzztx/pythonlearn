171eea0e124cc21c0507e901f40ecce9150026b9
tx <tangxiang007@gmail.com>
2017-08-26 08:20:31 -0700

    add animal.py and student.py

--- 7class/student.py
+++ 7class/student.py
@@ -2,14 +2,14 @@
 
 class Student(object):
 	def __init__(self,name,score):
-		self.name=name
-		self.score=score
+		self.__name=name
+		self.__score=score
 	def print_score(self):
-		print '%s:%s' % (self.name,self.score)
+		print '%s:%s' % (self.__name,self.__score)
 	def get_grade(self):
-		if self.score>=90:
+		if self.__score>=90:
 			return 'A'
-		elif self.score>=60:
+		elif self.__score>=60:
 			return 'B'
 		else:
 			return 'C'
@@ -18,4 +18,4 @@ if __name__=='__main__':
 	s1=Student('tx',100)
 	s1.print_score()
 	print s1.get_grade()
-
+	#print s1.__name
